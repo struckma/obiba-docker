936e61d5a6c72ef19cc934f748156e460be34064
stephan.struckmann <stephan.struckmann@uni-greifswald.de>
2020-02-19 14:42:56 +0100

    Fixed paths

--- drupal-manager.mk
+++ drupal-manager.mk
@@ -10,52 +10,52 @@ update-drupal:
 	docker-compose up -d
 
 backup-drupal-docker:
-	docker export obibadocker_drupal_1 | gzip > $(BACKUP_FOLDER)/drupal_images.gz
+	docker export obiba-docker_drupal_1 | gzip > $(BACKUP_FOLDER)/drupal_images.gz
 
 backup-drupal-default:
-	cd ./volumes/obibadocker_drupal_sites_default/_data/ && \
+	cd ./volumes/obiba-docker_drupal_sites_default/_data/ && \
 	tar -cvf $(BACKUP_FOLDER)/drupal_default.tar *
 
 backup-drupal-libraries:
-	cd ./volumes/obibadocker_drupal_sites_libraries/_data/ && \
+	cd ./volumes/obiba-docker_drupal_sites_libraries/_data/ && \
 	tar -cvf $(BACKUP_FOLDER)/drupal_libraries.tar *
 
 backup-drupal-modules:
-	cd ./volumes/obibadocker_drupal_sites_modules/_data/ && \
+	cd ./volumes/obiba-docker_drupal_sites_modules/_data/ && \
 	tar -cvf $(BACKUP_FOLDER)/drupal_modules.tar *
 
 backup-drupal-themes:
-	cd ./volumes/obibadocker_drupal_sites_themes/_data/ && \
+	cd ./volumes/obiba-docker_drupal_sites_themes/_data/ && \
 	tar -cvf $(BACKUP_FOLDER)/drupal_themes.tar *
 
 backup-drupal-vendor:
-	cd ./volumes/obibadocker_drupal_sites_vendor/_data/ && \
+	cd ./volumes/obiba-docker_drupal_sites_vendor/_data/ && \
 	tar -cvf $(BACKUP_FOLDER)/drupal_vendor.tar *
 
 restore-drupal-docker:
-	zcat $(BACKUP_FOLDER)/drupal.gz | docker import - obibadocker_drupal_1
+	zcat $(BACKUP_FOLDER)/drupal.gz | docker import - obiba-docker_drupal_1
 
 restore-drupal-default:
-	cp $(BACKUP_FOLDER)/drupal_default.tar ./volumes/obibadocker_drupal_sites_default/_data/ && \
-    cd ./volumes/obibadocker_drupal_sites_default/_data/ && \
+	cp $(BACKUP_FOLDER)/drupal_default.tar ./volumes/obiba-docker_drupal_sites_default/_data/ && \
+    cd ./volumes/obiba-docker_drupal_sites_default/_data/ && \
     tar -xvf drupal_default.tar
 
 restore-drupal-libraries:
-	cp $(BACKUP_FOLDER)/drupal_libraries.tar ./volumes/obibadocker_drupal_sites_libraries/_data/ && \
-    cd ./volumes/obibadocker_drupal_sites_libraries/_data/ && \
+	cp $(BACKUP_FOLDER)/drupal_libraries.tar ./volumes/obiba-docker_drupal_sites_libraries/_data/ && \
+    cd ./volumes/obiba-docker_drupal_sites_libraries/_data/ && \
     tar -xvf drupal_libraries.tar
 
 restore-drupal-modules:
-	cp $(BACKUP_FOLDER)/drupal_modules.tar ./volumes/obibadocker_drupal_sites_modules/_data/ && \
-    cd ./volumes/obibadocker_drupal_sites_modules/_data/ && \
+	cp $(BACKUP_FOLDER)/drupal_modules.tar ./volumes/obiba-docker_drupal_sites_modules/_data/ && \
+    cd ./volumes/obiba-docker_drupal_sites_modules/_data/ && \
     tar -xvf drupal_modules.tar
 
 restore-drupal-themes:
-	cp $(BACKUP_FOLDER)/drupal_themes.tar ./volumes/obibadocker_drupal_sites_themes/_data/ && \
-    cd ./volumes/obibadocker_drupal_sites_themes/_data/ && \
+	cp $(BACKUP_FOLDER)/drupal_themes.tar ./volumes/obiba-docker_drupal_sites_themes/_data/ && \
+    cd ./volumes/obiba-docker_drupal_sites_themes/_data/ && \
     tar -xvf drupal_themes.tar
 
 restore-drupal-vendor:
-	cp $(BACKUP_FOLDER)/drupal_vendor.tar ./volumes/obibadocker_drupal_sites_vendor/_data/ && \
-    cd ./volumes/obibadocker_drupal_sites_vendor/_data/ && \
+	cp $(BACKUP_FOLDER)/drupal_vendor.tar ./volumes/obiba-docker_drupal_sites_vendor/_data/ && \
+    cd ./volumes/obiba-docker_drupal_sites_vendor/_data/ && \
     tar -xvf drupal_vendor.tar
